65bc6ca639a3ecf95d9f8fa3ce78f2f19b9e6ea5
webdevmission <120027977+webdevmission@users.noreply.github.com>
2023-05-11 14:03:24 +0200

    Setup tailwind (#1)
    * setup custom classes for tailwind
    
    * delete npm and config tailwindcss
    
    * finish initial setup

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,21 @@
 module.exports = {
   content: ["./src/index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
   theme: {
+    fontFamily: {
+      sans: ["poppins", "sans-serif"],
+      serif: ["serif"],
+    },
+    colors: {
+      "d-dark": "#FBFBFB",
+      "d-medium": "#3A4151",
+      "d-light": "#FBFBFB",
+      "grad-yellow": "#FFD162",
+      "grad-red": "#FF99C3",
+      "grad-green": "#3EF3E8",
+      "grad-neon-yellow": "#FFD162",
+      "grad-cyan": "#3EF3E8",
+      "grad-blue": "#3A4AE4",
+    },
     extend: {},
   },
   plugins: [],
