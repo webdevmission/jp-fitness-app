d401d834d9be28b75aa5628b7b34c1686ff73421
webdevmission <webdevmission@gmail.com>
2023-05-31 10:15:15 +0200

    setup browse page

---
+++ src/pages/browse/components/index.jsx
@@ -0,0 +1,12 @@
+import DefaultLayout from "./components/DefaultLayout";
+import NavBar from "../../global/components/NavBar";
+
+const Browse = () => {
+  return (
+    <DefaultLayout>
+      <NavBar />
+    </DefaultLayout>
+  );
+};
+
+export default Browse;
--- src/pages/dashboard/components/DashboardLayout/index.jsx
+++ src/pages/dashboard/components/DashboardLayout/index.jsx
@@ -1,5 +1,5 @@
-const DashboardLayout = ({ children }) => {
-  return <section className="h-screen bg-d-dark">{children}</section>;
+const DefaultLayout = ({ children }) => {
+  return <section className=" bg-d-dark">{children}</section>;
 };
 
-export default DashboardLayout;
+export default DefaultLayout;
